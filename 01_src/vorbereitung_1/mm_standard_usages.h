e8530758cc46e7e41fd83a9e3be763cf4b15e003
Mirko Matošin <mami1036@hs-karlsruhe.de>
2017-02-20 18:43:20 +0100

    first test with fork() pid_t is the process id added some examples for classes and stuff, just to test smth added directories

---
+++ 01_src/vorbereitung_1/mm_standard_usages.h
@@ -0,0 +1,26 @@
+//
+// Created by mirko on 19.02.17.
+//
+
+#ifndef LABOR_SWE_2_MM_STANDARD_USAGES_H
+#define LABOR_SWE_2_MM_STANDARD_USAGES_H
+// synonyme for elementar data types
+
+#include <cmath>
+
+typedef signed char    Int8;
+typedef unsigned char  UInt8;
+typedef signed short   Int16;
+typedef unsigned short UInt16;
+typedef signed int     Int32;
+typedef unsigned int   UInt32;
+typedef double         f64;
+typedef float          f32;
+
+
+// Definition fuer nullpointer und null character
+//#define NULLPTR 0
+//#define NULLCHAR 0
+
+
+#endif //LABOR_SWE_2_MM_STANDARD_USAGES_H
